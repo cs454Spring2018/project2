0df03ffcb01a4bc4148aec2569d7bc717519da64
Nathan Kamm <eathotlead@gmail.com>
2018-03-30 22:05:18 -0700

    Update num3.js

--- num3.js
+++ num3.js
@@ -10,7 +10,7 @@ const readFile = async name => {
     map[index] = graph[index].split('  ')
   }
 
-  return [map, state]
+  return [map, state, acceptingStates]
 }
 
 const mergeArray = (sArray, bArray) => {
@@ -20,9 +20,8 @@ const mergeArray = (sArray, bArray) => {
 }
 
 
-const mergeDFA = data => {
-  let mainDFA = {}, s = new Set(), merged;
-  let [ [ dfa, states ], [ dfa1, states1 ] ] = data
+const mergeDFA = (dfa, dfa1) => {
+  let mainDFA = [], newStates = {}, s = new Set(), merged;
 
   if (Object.keys(dfa).length === 0)  return dfa1
   if (Object.keys(dfa1).length === 0) return dfa
@@ -30,41 +29,77 @@ const mergeDFA = data => {
   merged = mergeArray(dfa[0], dfa1[0])
 
   let track = [...merged]
-  mainDFA['00'] = merged
+  mainDFA.push(merged)
+  newStates['0|0'] = 0;
+  s.add('0|0')
+  let stateTrack = 1;
 
   while (track.length !== 0) {
     let val = track.shift();
+    val = val.replace(' ', '')
     if (!s.has(val)) {
+      newStates[val] = stateTrack++;
       let state1 = val.match(/(\d*)\|/)[1]
       let state2 = val.match(/\|(\d*)/)[1]
       merged = mergeArray(dfa[state1], dfa1[state2]);
-      mainDFA[`${state1}${state2}`] = merged;
+      mainDFA.push(merged)
       track = [...track, ...merged]
       s.add(val);
     }
   }
-  return mainDFA
+  return [ mainDFA, newStates ]
 }
 
-const cleanDFA = dfa => {
-  let newDFA = {}
-  for (item in dfa)
-    newDFA[parseInt(item)] = cleanArray(dfa[item])
-  return newDFA
+
+const normalizeDFA = ([mainDFA, newStates]) => {
+  //n^2
+  for (let i = 0; i < mainDFA.length; i++) {
+    fixState(mainDFA[i], newStates)
+  }
+
 }
 
-const cleanArray = array => {
-  return array.map(item => {
-    let state1 = item.match(/(\d*)\|/)[1]
-    let state2 = item.match(/\|(\d*)/)[1]
-    return parseInt(`${state1}${state2}`)
-  })
+
+const fixState = (currentTransition, newStates) => {
+  let data = ''
+  for (let i = 0; i < currentTransition.length; i++) {
+    data += `${newStates[currentTransition[i].replace(' ', '')]}\t`
+  }
+  console.log(data)
+}
+
+const createAcceptingStates = (accept, accept1, length) => {
+  accept = accept.split(' ')
+  accept1 = accept1.split(' ')
+  let data = new Set([...accept, ...accept1])
+  console.log(data)
+  let str = ''
+  for (let i = 0; i < length; i++){
+    if (isIn(i, data)) {str += `${i} `}
+  }
+  console.log(str)
+}
+
+const isIn = (iValue, data) => {
+  iValue = iValue.toString()
+
+  for (let i = 0; i < iValue.length; i++) {
+    if (!(data.has(iValue[i]))) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
 (async () => {
-  let data = await Promise.all([readFile('dfa1.txt'), readFile('dfa2.txt')])
-  let union = mergeDFA(data)
-  let cleaned = cleanDFA(union)
-  console.log(cleaned)
+  let [ [ dfa, states, acceptingStates ], [ dfa1, states1, acceptingStates1 ] ] =
+      await Promise.all([readFile('dfa1.txt'), readFile('dfa2.txt')])
+  let union = mergeDFA(dfa, dfa1)
+  console.log(union)
+  createAcceptingStates(acceptingStates, acceptingStates1, union[0].length)
+  normalizeDFA(union)
+
+
+
 })()
